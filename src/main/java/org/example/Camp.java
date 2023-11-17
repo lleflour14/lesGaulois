0db8b1e24900bf132b4b2c0ddeb429812961ee63
yanni <yann.irz@gmail.com>
2023-11-17 10:48:03 +0100

    Romain/Camp

--- src/main/java/org/example/Camp.java
+++ src/main/java/org/example/Camp.java
@@ -1,4 +1,28 @@
 package org.example;
+import java.util.ArrayList;
 
 public class Camp {
+    private ArrayList<Romain> lesRomains;
+    private String nom;
+    private Romain chef;
+
+    public Camp(ArrayList<Romain> lesRomains, String nom, Romain chef){
+        this.lesRomains = lesRomains;
+        this.nom=nom;
+        this.chef = chef;
+    }
+
+    @Override
+    public String toString() {
+        return "Camp{" +
+                ", nom='" + nom + '\'' +
+                ", chef=" + chef + + '\'' +
+                "lesRomains=" + lesRomains +
+                '}';
+    }
+
+    public void ajouterRomain(Romain r){
+        lesRomains.add(r);
+
+    }
 }
