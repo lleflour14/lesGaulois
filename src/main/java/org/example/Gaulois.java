77e01091824a1734b3ab79c3ff900fb457725f41
lleflour <louise.leflour@sfr.fr>
2023-11-17 10:48:36 +0100

    création humain

--- src/main/java/org/example/Gaulois.java
+++ src/main/java/org/example/Gaulois.java
@@ -1,5 +1,21 @@
 package org.example;
 
-public class Gaulois {
+public class Gaulois extends Humain{
 
+    private String metier;
+
+    public Gaulois(String nom, int force, String metier) {
+        super(nom, force);
+        this.metier = metier;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                " - metier= " + metier;
+    }
+
+    public String getMetier() {
+        return metier;
+    }
 }
