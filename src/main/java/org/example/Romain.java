9123416be806fba13253b34b8b6a7ac7c4e15f21
yanni <yann.irz@gmail.com>
2023-11-17 10:57:36 +0100

    Romain/Camp

--- src/main/java/org/example/Romain.java
+++ src/main/java/org/example/Romain.java
@@ -13,5 +13,8 @@ public class Romain extends Humain{
         return super.toString() + "- grade= " + graderomain;
     }
 
+    public Grade getGraderomain() {
+        return graderomain;
+    }
 }
 
